8d1024141e06a21f941c2d82f83085a8703d4a7a
illidan6727 <113511019+illidan6727@users.noreply.github.com>
2022-09-28 16:21:16 +0500

    Add files via upload

---
+++ mustaqil ishlar/3-amaliy ish.py
@@ -0,0 +1,11 @@
+
+a=int(input("birinchi sonni kiriting "))
+b=int(input("ikkinchi sonni kiriting "))
+if (a%2==1):
+    print(f"birinchi son toq {a}")
+elif (b%2==1):
+    print(f"ikkinchi son toq {b}")
+else:
+    print ("boshqa son kiriting")
+    
+    
