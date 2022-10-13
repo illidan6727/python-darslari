3a005bd159c4bb8e86947fae9f2429775aaaee8d
illidan6727 <113511019+illidan6727@users.noreply.github.com>
2022-10-13 13:21:52 +0500

    Add files via upload

---
+++ mustaqil ishlar/2_amaliy ish.py
@@ -0,0 +1,16 @@
+
+
+
+a=int(input())
+a=a+a/100*45
+print(a)
+daromad_soligi=a//100*17
+kasaba_soligi=a/100*1.5
+nafaqa_soligi=a/100
+print(a-daromad_soligi-kasaba_soligi-nafaqa_soligi)
+
+
+
+
+
+
